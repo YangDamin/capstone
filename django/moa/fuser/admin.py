d61e25b77211ad16ea6ded33baee8fbc5964f009
Seongjoo <tloc0e04@gmail.com>
2021-05-24 17:25:12 +0900

    no message

--- django/moa/fuser/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Fuser
-
-# Register your models here.
-class FuserAdmin(admin.ModelAdmin):
-    # pass
-    list_display = ('user_id', 'password', 'user_name', 'user_email')
-
-admin.site.register(Fuser, FuserAdmin)
--- django/moa/management/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path(r'index/' , views.cntcoupon, name='index'),
-    path('logout/', views.logout, name='logout'),
-]
